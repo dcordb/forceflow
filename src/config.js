03f28ad72e7376fee2c8676e168acfd03ef14c86
Daniel Enrique <dcordb97@gmail.com>
2023-09-06 21:42:02 +0200

    feat: add general config.

--- src/config.js
+++ src/config.js
@@ -2,17 +2,46 @@ import path from "node:path";
 import os from "node:os";
 import { fileOrDirExists } from "./utils.js";
 import { mkdir } from "node:fs/promises";
+import * as fs from "node:fs";
 
 const CONFIG_DIR = path.join(os.homedir(), ".forceflow");
 const DAEMON_FILE = path.join(CONFIG_DIR, "daemon.json");
 const LANGS_FILE = path.join(CONFIG_DIR, "langs.json");
+const CONFIG_FILE = path.join(CONFIG_DIR, "config.json");
 
 async function createConfigDir() {
   const exists = await fileOrDirExists(CONFIG_DIR);
 
-  if (!exists) await mkdir(CONFIG_DIR);
+  if (!exists) {
+    await mkdir(CONFIG_DIR);
+    const obj = { code_dir: CODE_DIR };
+    fs.writeFileSync(CONFIG_FILE, JSON.stringify(obj, null, 4));
+  }
 }
 
-createConfigDir();
+await createConfigDir();
 
-export { CONFIG_DIR, DAEMON_FILE, LANGS_FILE };
+function loadConfig() {
+  let data = null;
+
+  try {
+    data = fs.readFileSync(CONFIG_FILE, { encoding: "utf8" });
+  } catch (e) {
+    return {};
+  }
+
+  return JSON.parse(data);
+}
+
+const configData = loadConfig();
+const CODE_DIR = configData.code_dir || path.join(CONFIG_DIR, "code");
+
+async function createCodeDir(dir) {
+  const exists = await fileOrDirExists(dir);
+
+  if (!exists) await mkdir(dir);
+}
+
+await createCodeDir(CODE_DIR);
+
+export { CONFIG_DIR, DAEMON_FILE, LANGS_FILE, CODE_DIR };
